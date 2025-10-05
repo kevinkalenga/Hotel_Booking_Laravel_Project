d81eafce3fb15444af8dcc4b9330da4f4eef894b
kevin <kevinkalenga10@gmail.com>
2025-10-06 00:09:58 +0200

    Admin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+         'admin' => \App\Http\Middleware\Admin::class
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
