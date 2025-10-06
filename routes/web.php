d37f9e0b55bb56ee3b486975c5b5a754bf55b617
kevin <kevinkalenga10@gmail.com>
2025-10-07 01:41:29 +0200

    Mastering the template and working with home and about page

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\AdminProfilController;
+use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\AboutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-// /* Admin */
-// Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');;
-// Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
-// Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
-// Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
-// Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
-// Route::post('/admin/forget-password-submit', [AdminLoginController::class, 'forget_password_submit'])->name('admin_forget_password_submit');
-// Route::get('/admin/reset-password/{token}/{email}', [AdminLoginController::class, 'reset_password'])->name('admin_reset_password');
-// Route::post('/admin/reset-password/{token}/{email}', [AdminLoginController::class, 'reset_password_submit'])->name('admin_reset_password_submit');
+
+/* ------------------------- Front--------------------- */
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+
 
 /* ---------------------- Admin ---------------------- */
 
