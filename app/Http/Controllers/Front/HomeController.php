e1ea35b226488c55a892fcc9bcc0084a113d8b10
kevin <kevinkalenga10@gmail.com>
2025-10-10 01:03:01 +0200

    Feature - Part5

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use App\Models\Feature;
 
 class HomeController extends Controller
 {
     public function index()
     {
           $slide_all = Slider::get();
-          return view('front.home', compact('slide_all'));
+          $feature_all = Feature::get();
+          return view('front.home', compact('slide_all', 'feature_all'));
     }
 }
