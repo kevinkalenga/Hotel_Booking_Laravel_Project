6b5c104c36c66780c33376e44910d47d745c50ea
kevin <kevinkalenga10@gmail.com>
2025-10-11 00:43:13 +0200

    Blog - Part2

--- app/Http/Controllers/Admin/AdminPostController.php
+++ app/Http/Controllers/Admin/AdminPostController.php
@@ -12,4 +12,39 @@ class AdminPostController extends Controller
         $posts = Post::get();
         return view('admin.post_view', compact('posts'));
     }
+
+
+    public function add()
+    {
+        return view('admin.post_add');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'photo' => ['required', 'image', 'mimes:jpg,jpeg,png,gif', 'max:2048'],
+            'heading' => 'required',
+            'short_content' => 'required',
+            'content' => 'required',
+        ]);
+
+        $final_name = null;
+
+        if ($request->hasFile('photo')) {
+            $ext = $request->file('photo')->extension();
+            $final_name = time() . '.' . $ext;
+            $request->file('photo')->move(public_path('uploads/'), $final_name);
+        }
+
+        $obj = new Post();
+        $obj->photo = $final_name;
+        $obj->heading = $request->heading;
+        $obj->short_content = $request->short_content;
+        $obj->content = $request->content;
+        $obj->total_view = 1;
+        $obj->save();
+
+        return redirect()->back()->with('success', 'Post added successfully');
+    }
+
 }
