a1cbd1ef91203c300bef405564a75006d69ab4ba
kevin <kevinkalenga10@gmail.com>
2025-10-17 00:55:06 +0200

    Page - Video Gallery - Part2

--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -140,7 +140,9 @@
                                 @if($global_page_data->photo_gallery_status == 1)
                                  <li><a href="{{route('photo_gallery')}}">{{$global_page_data->photo_gallery_heading}}</a></li>
                                 @endif
-                                <li><a href="{{route('video_gallery')}}">Video Gallery</a></li>
+                                @if($global_page_data->photo_gallery_status == 1)
+                                 <li><a href="{{route('video_gallery')}}">{{$global_page_data->video_gallery_heading}}</a></li>
+                                @endif
                                 <li><a href="{{route('blog')}}">Blog</a></li>
                                  @if($global_page_data->contact_status == 1)
                                   <li><a href="{{route('contact')}}"> {{$global_page_data->contact_heading}}</a></li>
