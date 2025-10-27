f265f4b918da0ac1239e55d7c90310566e779a56
kevin <kevinkalenga10@gmail.com>
2025-10-27 01:39:07 +0100

    Profile - Part2

--- app/Http/Controllers/Customer/CustomerProfileController.php
+++ app/Http/Controllers/Customer/CustomerProfileController.php
@@ -14,4 +14,66 @@ class CustomerProfileController extends Controller
     {
         return view('customer.profile');
     }
+
+
+
+    public function profile_submit(Request $request)
+    {
+       $request->validate([
+             'name' => 'required',
+             'email' => 'required|email',
+             'password' => 'nullable|min:6|confirmed',
+        ]);
+
+        
+        
+       
+        $customer_data = Customer::find(Auth::guard('customer')->id());
+        // Mettre à jour le mot de passe seulement si rempli
+        if ($request->password) {
+              $customer_data->password = Hash::make($request->password);
+        }
+      
+        //  photo
+
+        if ($request->hasFile('photo')) {
+           $request->validate([
+            'photo' => ['image', 'mimes:jpg,jpeg,png,gif', 'max:2048'],
+           ]);
+
+            // Supprime l'ancienne photo seulement si ce n'est pas une photo par défaut
+            $defaultPhotos = ['user.jpg', 'default.png'];
+            if ($customer_data->photo && !in_array($customer_data->photo, $defaultPhotos) && file_exists(public_path('uploads/' . $customer_data->photo))) {
+                unlink(public_path('uploads/' . $customer_data->photo));
+            }
+
+             // Sauvegarde la nouvelle photo
+             $final_name = 'customer_' . time() . '.' . $request->photo->extension();
+             $request->photo->move(public_path('uploads'), $final_name);
+             $customer_data->photo = $final_name;
+        }
+
+
+       
+        
+        
+  
+
+      
+        // Update
+        $customer_data->name  = $request->name;
+        $customer_data->email = $request->email;
+        $customer_data->phone = $request->phone;
+        $customer_data->country = $request->country;
+        $customer_data->address = $request->address;
+        $customer_data->state = $request->state;
+        $customer_data->city = $request->city;
+        $customer_data->zip = $request->zip;
+        $customer_data->save();
+
+        Auth::guard('customer')->setUser($customer_data->fresh());
+
+        return redirect()->back()->with('success', 'Profile updated successfully.');
+
+    }
 }
--- resources/views/customer/profile.blade.php
+++ resources/views/customer/profile.blade.php
@@ -26,22 +26,80 @@
                                                 <input type="file" class="mt_10" name="photo">
                                             </div>
                                             <div class="col-md-9">
-                                                <div class="mb-4">
-                                                    <label class="form-label">Name *</label>
-                                                    <input type="text" class="form-control" name="name" value="{{ Auth::guard('customer')->user()->name }}">
+                                                
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                            <label class="form-label">Name *</label>
+                                                            <input type="text" class="form-control" name="name" value="{{ Auth::guard('customer')->user()->name }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                           <label class="form-label">Email *</label>
+                                                           <input type="text" class="form-control" name="email" value="{{ Auth::guard('customer')->user()->email }}">
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="mb-4">
-                                                    <label class="form-label">Email *</label>
-                                                    <input type="text" class="form-control" name="email" value="{{ Auth::guard('customer')->user()->email }}">
+
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                            <label class="form-label">Phone </label>
+                                                            <input type="text" class="form-control" name="phone" value="{{ Auth::guard('customer')->user()->phone }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                           <label class="form-label">Country </label>
+                                                           <input type="text" class="form-control" name="country" value="{{ Auth::guard('customer')->user()->country }}">
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="mb-4">
-                                                    <label class="form-label">Password</label>
-                                                    <input type="password" class="form-control" name="password">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                            <label class="form-label">Address </label>
+                                                            <input type="text" class="form-control" name="address" value="{{ Auth::guard('customer')->user()->address }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                           <label class="form-label">State </label>
+                                                           <input type="text" class="form-control" name="state" value="{{ Auth::guard('customer')->user()->state }}">
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                                <div class="mb-4">
-                                                    <label class="form-label">Retype Password</label>
-                                                    <input type="password" class="form-control" name="password_confirmation">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                            <label class="form-label">City </label>
+                                                            <input type="text" class="form-control" name="city" value="{{ Auth::guard('customer')->user()->city }}">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="mb-4">
+                                                           <label class="form-label">Zip </label>
+                                                           <input type="text" class="form-control" name="zip" value="{{ Auth::guard('customer')->user()->zip }}">
+                                                        </div>
+                                                    </div>
                                                 </div>
+                                                <div class="row">
+                                                   <div class="col-md-6">
+                                                     <div class="mb-4">
+                                                        <label class="form-label">Password</label>
+                                                        <input type="password" class="form-control" name="password">
+                                                     </div>
+                                                   </div>
+                                                   <div class="col-md-6">
+                                                       <div class="mb-4">
+                                                         <label class="form-label">Retype Password</label>
+                                                         <input type="password" class="form-control" name="password_confirmation">
+                                                        </div>
+                                                   </div>
+                                                </div>
+                                            
+                                              
                                                 <div class="mb-4">
                                                     <label class="form-label"></label>
                                                     <button type="submit" class="btn btn-primary">Update</button>
