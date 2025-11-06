0f28510952e4b5eddce73c4c4c68c2a3156c6548
kevin <kevinkalenga10@gmail.com>
2025-11-06 08:55:51 +0100

    Admin - Customer section - Part1

--- app/Http/Controllers/Customer/CustomerHomeController.php
+++ app/Http/Controllers/Customer/CustomerHomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Customer;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
@@ -10,8 +11,10 @@ class CustomerHomeController extends Controller
 {
     public function index()
     {
-        $total_completed_orders = Order::where('status', 'Completed')->count();
-        $total_pending_orders = Order::where('status', 'Pending')->count();
+        $total_completed_orders = Order::where('status', 'Completed')
+        ->where('customer_id', Auth::guard('customer')->user()->id)->count();
+        $total_pending_orders = Order::where('status', 'Pending')
+        ->where('customer_id', Auth::guard('customer')->user()->id)->count();
         return view('customer.home', compact('total_completed_orders', 'total_pending_orders'));
     }
 }
